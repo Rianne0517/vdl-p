63c62091ee8c5cb931ccfd81285b42e8a95b3e97
werra11 <luukbos1@hotmail.com>
2020-04-14 23:08:17 +0200

    .

--- navbar.php
+++ navbar.php
@@ -1,10 +1,8 @@
 <?php
 $pages = array();
 $pages["home.php"] = "Home";
-$pages["cases.php"] = "Cases";
 $pages["watwedoen.php"] = "Wat we doen";
 $pages["overons.php"] = "Over ons";
-$pages["contact.php"] = "Contact";
 $pages["contact.php"] = "Contact";
 $pages["vacatures.php"] = "Vacatures";
 ?>
